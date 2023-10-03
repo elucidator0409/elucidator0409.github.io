42b9faca7cd8ceda4ed714175dec281b96bb96eb
Dbao <leducbao2k1@gmail.com>
2023-10-03 17:19:46 +0700

    change button

---
+++ js/dataUser.js
@@ -0,0 +1,502 @@
+export const myData =[ 
+    {
+      "first_name": "Sonia",
+      "email": "szoane0@google.co.uk",
+      "phone": "507-132-2609"
+    },
+    {
+      "first_name": "Ingeborg",
+      "email": "igoseling1@dropbox.com",
+      "phone": "986-740-9004"
+    },
+    {
+      "first_name": "Caitlin",
+      "email": "cmcpike2@mail.ru",
+      "phone": "794-364-3152"
+    },
+    {
+      "first_name": "Cobbie",
+      "email": "ckos3@t.co",
+      "phone": "959-709-9580"
+    },
+    {
+      "first_name": "Stavro",
+      "email": "showcroft4@plala.or.jp",
+      "phone": "310-510-0277"
+    },
+    {
+      "first_name": "Hannie",
+      "email": "hmolfino5@unicef.org",
+      "phone": "305-571-9789"
+    },
+    {
+      "first_name": "Erda",
+      "email": "efiggins6@printfriendly.com",
+      "phone": "630-641-8497"
+    },
+    {
+      "first_name": "Drusy",
+      "email": "draynes7@cdc.gov",
+      "phone": "527-296-2512"
+    },
+    {
+      "first_name": "Lani",
+      "email": "larnaldy8@salon.com",
+      "phone": "265-296-5617"
+    },
+    {
+      "first_name": "Lew",
+      "email": "locahill9@msu.edu",
+      "phone": "233-931-9151"
+    },
+    {
+      "first_name": "Brandea",
+      "email": "bstraughana@cmu.edu",
+      "phone": "285-369-5068"
+    },
+    {
+      "first_name": "Jerry",
+      "email": "jeverardb@dmoz.org",
+      "phone": "376-596-4756"
+    },
+    {
+      "first_name": "Barr",
+      "email": "bandrewarthac@seesaa.net",
+      "phone": "436-744-3882"
+    },
+    {
+      "first_name": "Coreen",
+      "email": "clongwoodd@exblog.jp",
+      "phone": "639-616-8645"
+    },
+    {
+      "first_name": "Kelwin",
+      "email": "kkilne@arizona.edu",
+      "phone": "242-193-0069"
+    },
+    {
+      "first_name": "Deny",
+      "email": "dbaxendalef@theguardian.com",
+      "phone": "794-892-1634"
+    },
+    {
+      "first_name": "Shaine",
+      "email": "sgregineg@shutterfly.com",
+      "phone": "757-460-1486"
+    },
+    {
+      "first_name": "Jeromy",
+      "email": "jhemshallh@amazon.com",
+      "phone": "685-202-7724"
+    },
+    {
+      "first_name": "Branden",
+      "email": "bdainteri@usnews.com",
+      "phone": "417-721-9461"
+    },
+    {
+      "first_name": "Koo",
+      "email": "kmasonj@eventbrite.com",
+      "phone": "967-933-3407"
+    },
+    {
+      "first_name": "Pavia",
+      "email": "pbartaluccik@engadget.com",
+      "phone": "299-864-8089"
+    },
+    {
+      "first_name": "Denver",
+      "email": "dbrokel@blogs.com",
+      "phone": "690-695-6607"
+    },
+    {
+      "first_name": "Normie",
+      "email": "nbeatsonm@google.it",
+      "phone": "580-929-6948"
+    },
+    {
+      "first_name": "Dayna",
+      "email": "dschrineln@auda.org.au",
+      "phone": "809-985-5404"
+    },
+    {
+      "first_name": "Clevey",
+      "email": "cchalfonto@geocities.jp",
+      "phone": "404-165-0258"
+    },
+    {
+      "first_name": "Carter",
+      "email": "clewryp@tinyurl.com",
+      "phone": "666-768-4239"
+    },
+    {
+      "first_name": "Blaine",
+      "email": "bwatkissq@nba.com",
+      "phone": "256-357-2064"
+    },
+    {
+      "first_name": "Jobey",
+      "email": "jmartinezr@patch.com",
+      "phone": "694-615-1047"
+    },
+    {
+      "first_name": "Lefty",
+      "email": "lbletsors@google.com.hk",
+      "phone": "307-138-8302"
+    },
+    {
+      "first_name": "Vivia",
+      "email": "vstakert@hatena.ne.jp",
+      "phone": "172-115-8717"
+    },
+    {
+      "first_name": "Nelle",
+      "email": "nmacilwricku@godaddy.com",
+      "phone": "347-865-9711"
+    },
+    {
+      "first_name": "Piotr",
+      "email": "pextilv@hao123.com",
+      "phone": "640-318-7929"
+    },
+    {
+      "first_name": "Thorvald",
+      "email": "tdegiorgiw@forbes.com",
+      "phone": "568-744-9515"
+    },
+    {
+      "first_name": "Brier",
+      "email": "bwellingsx@globo.com",
+      "phone": "112-910-6453"
+    },
+    {
+      "first_name": "Matti",
+      "email": "mgravesy@apache.org",
+      "phone": "821-666-9724"
+    },
+    {
+      "first_name": "Deva",
+      "email": "dmcmeylerz@drupal.org",
+      "phone": "856-770-4218"
+    },
+    {
+      "first_name": "Birgitta",
+      "email": "bolone10@epa.gov",
+      "phone": "721-382-7443"
+    },
+    {
+      "first_name": "Dottie",
+      "email": "dtocqueville11@networksolutions.com",
+      "phone": "510-392-1346"
+    },
+    {
+      "first_name": "Jarrid",
+      "email": "jmcgrouther12@domainmarket.com",
+      "phone": "317-233-5103"
+    },
+    {
+      "first_name": "Laurella",
+      "email": "lbrigstock13@intel.com",
+      "phone": "402-538-4143"
+    },
+    {
+      "first_name": "Linus",
+      "email": "ljaze14@blogtalkradio.com",
+      "phone": "854-561-0454"
+    },
+    {
+      "first_name": "Matthiew",
+      "email": "mscourfield15@sakura.ne.jp",
+      "phone": "581-786-4054"
+    },
+    {
+      "first_name": "Jonah",
+      "email": "jgenders16@usda.gov",
+      "phone": "756-662-0550"
+    },
+    {
+      "first_name": "Magdaia",
+      "email": "mbuckland17@devhub.com",
+      "phone": "510-372-8489"
+    },
+    {
+      "first_name": "Rorie",
+      "email": "rdidsbury18@google.ru",
+      "phone": "153-494-8007"
+    },
+    {
+      "first_name": "Paxon",
+      "email": "pstoll19@google.pl",
+      "phone": "827-412-1355"
+    },
+    {
+      "first_name": "Leanora",
+      "email": "lalpine1a@cbsnews.com",
+      "phone": "715-940-9707"
+    },
+    {
+      "first_name": "Madison",
+      "email": "mwyles1b@berkeley.edu",
+      "phone": "154-412-3069"
+    },
+    {
+      "first_name": "Isidora",
+      "email": "imashro1c@github.com",
+      "phone": "733-384-5738"
+    },
+    {
+      "first_name": "Royal",
+      "email": "rmaffi1d@imageshack.us",
+      "phone": "473-548-3425"
+    },
+    {
+      "first_name": "Carlita",
+      "email": "cattreed1e@alexa.com",
+      "phone": "356-418-6708"
+    },
+    {
+      "first_name": "Esmeralda",
+      "email": "ehembling1f@printfriendly.com",
+      "phone": "583-902-8173"
+    },
+    {
+      "first_name": "Hersch",
+      "email": "hridehalgh1g@spotify.com",
+      "phone": "594-558-1354"
+    },
+    {
+      "first_name": "Chiarra",
+      "email": "croelofs1h@naver.com",
+      "phone": "581-979-1832"
+    },
+    {
+      "first_name": "Melita",
+      "email": "mflatt1i@howstuffworks.com",
+      "phone": "400-735-7969"
+    },
+    {
+      "first_name": "Loren",
+      "email": "laubin1j@netscape.com",
+      "phone": "237-487-8337"
+    },
+    {
+      "first_name": "Ashlan",
+      "email": "acarrick1k@deliciousdays.com",
+      "phone": "288-796-8929"
+    },
+    {
+      "first_name": "Jorie",
+      "email": "jjagoe1l@guardian.co.uk",
+      "phone": "490-697-0628"
+    },
+    {
+      "first_name": "Pammie",
+      "email": "pbuick1m@tmall.com",
+      "phone": "514-622-3460"
+    },
+    {
+      "first_name": "Stefan",
+      "email": "shubbart1n@yellowpages.com",
+      "phone": "818-837-4798"
+    },
+    {
+      "first_name": "Perla",
+      "email": "pcousins1o@bluehost.com",
+      "phone": "363-538-2726"
+    },
+    {
+      "first_name": "Kermit",
+      "email": "knancarrow1p@technorati.com",
+      "phone": "691-703-1678"
+    },
+    {
+      "first_name": "Fanni",
+      "email": "fheditch1q@tuttocitta.it",
+      "phone": "943-712-7428"
+    },
+    {
+      "first_name": "Isadore",
+      "email": "icutmare1r@washingtonpost.com",
+      "phone": "393-955-8418"
+    },
+    {
+      "first_name": "Polly",
+      "email": "pdurnan1s@hud.gov",
+      "phone": "598-326-8525"
+    },
+    {
+      "first_name": "Isak",
+      "email": "igenney1t@histats.com",
+      "phone": "850-864-5067"
+    },
+    {
+      "first_name": "Duff",
+      "email": "dodriscoll1u@ed.gov",
+      "phone": "425-313-1526"
+    },
+    {
+      "first_name": "Craig",
+      "email": "clhommee1v@yahoo.com",
+      "phone": "871-697-0766"
+    },
+    {
+      "first_name": "Drew",
+      "email": "dmaccaull1w@fc2.com",
+      "phone": "553-537-1415"
+    },
+    {
+      "first_name": "Derry",
+      "email": "dwinsper1x@abc.net.au",
+      "phone": "758-931-1211"
+    },
+    {
+      "first_name": "Devlen",
+      "email": "dmarley1y@shareasale.com",
+      "phone": "789-134-2752"
+    },
+    {
+      "first_name": "Lyle",
+      "email": "lsaffin1z@smh.com.au",
+      "phone": "960-498-3253"
+    },
+    {
+      "first_name": "Addison",
+      "email": "akillwick20@example.com",
+      "phone": "463-895-4219"
+    },
+    {
+      "first_name": "Darrelle",
+      "email": "dmalatalant21@ft.com",
+      "phone": "151-309-4602"
+    },
+    {
+      "first_name": "Saba",
+      "email": "sdabourne22@bandcamp.com",
+      "phone": "880-203-2649"
+    },
+    {
+      "first_name": "Hubey",
+      "email": "hgoodbody23@edublogs.org",
+      "phone": "436-247-8407"
+    },
+    {
+      "first_name": "Gustavo",
+      "email": "gloft24@naver.com",
+      "phone": "551-346-2916"
+    },
+    {
+      "first_name": "Roda",
+      "email": "rkitchenman25@uol.com.br",
+      "phone": "320-169-2840"
+    },
+    {
+      "first_name": "Melvin",
+      "email": "mwhal26@hibu.com",
+      "phone": "225-127-0734"
+    },
+    {
+      "first_name": "Teri",
+      "email": "tbertholin27@fema.gov",
+      "phone": "671-260-4697"
+    },
+    {
+      "first_name": "Felice",
+      "email": "flively28@cnbc.com",
+      "phone": "482-141-3223"
+    },
+    {
+      "first_name": "Goran",
+      "email": "gleving29@qq.com",
+      "phone": "951-190-7724"
+    },
+    {
+      "first_name": "Scarface",
+      "email": "sandover2a@princeton.edu",
+      "phone": "320-936-9358"
+    },
+    {
+      "first_name": "Janice",
+      "email": "jblack2b@odnoklassniki.ru",
+      "phone": "202-563-7666"
+    },
+    {
+      "first_name": "Brandi",
+      "email": "btrowle2c@usa.gov",
+      "phone": "825-865-0007"
+    },
+    {
+      "first_name": "Fabian",
+      "email": "fmilazzo2d@istockphoto.com",
+      "phone": "663-432-7337"
+    },
+    {
+      "first_name": "Rhys",
+      "email": "rfruish2e@dropbox.com",
+      "phone": "704-657-1974"
+    },
+    {
+      "first_name": "Darbie",
+      "email": "dkneale2f@topsy.com",
+      "phone": "212-919-1429"
+    },
+    {
+      "first_name": "Windham",
+      "email": "wstork2g@freewebs.com",
+      "phone": "199-163-8402"
+    },
+    {
+      "first_name": "Morse",
+      "email": "mkliemchen2h@360.cn",
+      "phone": "590-707-0741"
+    },
+    {
+      "first_name": "Sholom",
+      "email": "sryland2i@columbia.edu",
+      "phone": "787-108-3311"
+    },
+    {
+      "first_name": "Son",
+      "email": "smangin2j@ebay.com",
+      "phone": "577-313-5044"
+    },
+    {
+      "first_name": "Freda",
+      "email": "fjanousek2k@zdnet.com",
+      "phone": "672-981-1607"
+    },
+    {
+      "first_name": "Dolorita",
+      "email": "dstrickler2l@de.vu",
+      "phone": "103-572-1088"
+    },
+    {
+      "first_name": "Cecelia",
+      "email": "chellwig2m@cnbc.com",
+      "phone": "602-356-9085"
+    },
+    {
+      "first_name": "Oran",
+      "email": "ocockshoot2n@topsy.com",
+      "phone": "458-511-1499"
+    },
+    {
+      "first_name": "Gerhardine",
+      "email": "gpanner2o@npr.org",
+      "phone": "823-262-8463"
+    },
+    {
+      "first_name": "Ambros",
+      "email": "adobbins2p@constantcontact.com",
+      "phone": "934-325-2442"
+    },
+    {
+      "first_name": "Genny",
+      "email": "gbaress2q@rambler.ru",
+      "phone": "889-216-6774"
+    },
+    {
+      "first_name": "Teresina",
+      "email": "tduchatel2r@edublogs.org",
+      "phone": "547-489-0741"
+    }
+  ]
---
+++ js/searchFilter.js
@@ -0,0 +1,12 @@
+// import {data} from "./dataUser"
+// const userInput = document.getElementById('inputUser'),
+// search =  document.getElementById('search'),
+// searchResult = document.getElementById('searchResult'),
+// nameUser = document.getElementById('name'),
+// phone = document.getElementById('phoneUser'),
+// email = document.getElementById('emailUser');
+// search.addEventListener('click', () => {
+//     let infoUser = userInput.value;
+//     let finalUrl = data;
+//     console.log(finalUrl)
+// })
